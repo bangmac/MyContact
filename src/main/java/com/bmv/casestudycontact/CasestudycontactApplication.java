27d831c425868d99613c347d5c45fbad93b287c3
bangmac <anhbexd@gmail.com>
2020-05-25 14:20:15 +0700

    create model

---
+++ src/main/java/com/bmv/casestudycontact/CasestudycontactApplication.java
@@ -0,0 +1,13 @@
+package com.bmv.casestudycontact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasestudycontactApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CasestudycontactApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bmv/casestudycontact/CasestudycontactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bmv.casestudycontact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasestudycontactApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
